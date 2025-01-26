b37aec43c1c30950f27945bf140b6243ba9b7501
Subhendu Singh <ssubhendu988@gmail.com>
2025-01-26 11:39:16 +0000

    done basic structure

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,9 +1,44 @@
 import express from "express";
+import z from "zod";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./utils/config";
+import { middleware } from "./middleware";
 
 const app = express();
 
+export const userInterface = z.object({
+    email: z.string({required_error: "Email field is requireed."}).email(),
+    password: z.string({required_error: "Password field is required."}).min(6),
+})
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/signup", (req, res) => {
+    const {success, data} = userInterface.safeParse(req.body);
+    
+    if(!success) {
+        res.status(400).json({error: data, msg: "Invalid input"});
+    }
+
+    console.log(data);
+});
+
+app.post("/signin", (req, res) => {
+    const {success, data} = userInterface.safeParse(req.body);
+    
+    if(!success) {
+        res.status(400).json({error: data, msg: "Invalid input"});
+    }
+
+    const token = jwt.sign({email: data?.email, id: 1}, JWT_SECRET);
+
+    res.status(200).json({token});
+});
+
+app.post("/room",middleware,  (req, res) => {
+    res.send("Room created!");
 });
 
 app.listen(3001, () => {
---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./utils/config";
+
+export function middleware(req: Request, res: Response, next: NextFunction) {
+    const token = req.headers["authorization"];
+
+    const decoded = jwt.verify(token as string, JWT_SECRET);
+
+    if(!decoded) {
+        res.status(401).json({msg: "Unauthorized"});
+    }
+
+    // @ts-ignore
+    req.userId = decoded.id;
+    // @ts-ignore
+    req.email = decoded.email;
+
+    next();
+}
