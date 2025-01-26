fba525ab475f6f3f375a7aa0c191979c03c38b29
Subhendu Singh <ssubhendu988@gmail.com>
2025-01-26 11:38:45 +0000

    done basic structure

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,25 @@
 import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (ws) => {
+wss.on("connection", (ws, request) => {
+
+    const url = request.url;
+    if(!url) {
+        return;
+    }
+
+    const queryParams = new URLSearchParams(url.split("?")[1]);
+    const token = queryParams.get("token");
+    const decoded = jwt.verify(token as string, "Hello World");
+
+    if(!decoded || !(decoded as JwtPayload).id) {
+        ws.send("Unauthorized");
+        ws.close();
+        return;
+    }
+
     ws.on("message", (data) => {
         ws.send(`Received: ${data}`);
     });
